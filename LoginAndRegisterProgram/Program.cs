018d4aefe131def39d0545bcf25fd250f57e5fce
urgen urgen <brannbackatobias@gmail.com>
2017-10-15 06:05:31 +0200

    first

--- LoginAndRegisterProgram/Program.cs
+++ LoginAndRegisterProgram/Program.cs
@@ -16,7 +16,7 @@ namespace LoginAndRegisterProgram
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoginForm());
         }
     }
 }
--- LoginAndRegisterProgram/RegisterForm.cs
+++ LoginAndRegisterProgram/RegisterForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace LoginAndRegisterProgram
 {
-    public partial class Form1 : Form
+    public partial class RegisterForm : Form
     {
-        public Form1()
+        public RegisterForm()
         {
             InitializeComponent();
         }
